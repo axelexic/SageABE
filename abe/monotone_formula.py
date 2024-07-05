dd4f2eb948c517a9e4469b4ac2c098bd8cd9dc8c
Yogesh Swami <axelexic@gmail.com>
2024-07-05 15:42:39 -0700

    Adding MSP code
    Adding code for monotone span program

--- abe/monotone_formula.py
+++ abe/monotone_formula.py
@@ -6,7 +6,9 @@ Convert a monotone boolean formulae into a
 
 import sage.logic.propcalc as prop
 import sage.logic.logicparser as lp
+from sage.rings.all import *
 from enum import Enum
+from sage.matrix.constructor import Matrix
 
 class NodeType(Enum):
     And =1
@@ -14,13 +16,20 @@ class NodeType(Enum):
     Not = 3
     Literal = 10
 
-class Node:
+class Formula:
     def __init__(self, ty : NodeType, name: str, left = None, right = None ) -> None:
         self._ty = ty
         self._name = name
         self._left = left
         self._right = right
         self._parent = None
+        self._span = None
+
+    def root(self):
+        if self.parent() == None:
+            return self
+        else:
+            return self.parent().root()
 
     def parent(self):
         return self._parent
@@ -29,6 +38,12 @@ class Node:
         old_parent = self._parent
         self._parent = parent
         return old_parent
+
+    def span(self):
+        return self._span
+
+    def set_span(self, val):
+        self._span = val
 
     def ty(self):
         return self._ty
@@ -42,6 +57,29 @@ class Node:
     def name(self):
         return self._name
 
+    def literals(self):
+        if self.ty() == NodeType.Literal:
+            return [self]
+        elif self.ty() == NodeType.Not:
+            return self.left().literals()
+        else:
+            lefts = self.left().literals()
+            rights = self.right().literals()
+            lefts.extend(rights)
+            return lefts
+
+    @staticmethod
+    def relabel_duplicates(root_node):
+        labels = root_node.literals()
+        uniques = map(lambda x : x.name(), labels)
+        indexes = dict(map(lambda x : (x, 0), uniques))
+
+        for x in labels:
+            assert isinstance(x, (LiteralNode))
+            index = indexes[x.name()]
+            indexes[x.name()] = index + 1
+            x.set_label(index)
+
 
     @staticmethod
     def from_formula(formula_string: str, is_monotone=False):
@@ -50,7 +88,13 @@ class Node:
             formula.convert_cnf()
         tree = formula.tree()
         tree = lp.apply_func(tree, formula.dist_not)
-        return Node.from_list(tree, is_monotone)
+        formula = Formula.from_list(tree, is_monotone)
+
+        if is_monotone:
+            Formula.relabel_duplicates(formula)
+            formula.set_span([1])
+
+        return formula
 
     @staticmethod
     def from_list(lst, is_monotone):
@@ -63,7 +107,7 @@ class Node:
                 if lst[1] == None and lst[2] == None:
                     raise ValueError("Negation of non-existent literal")
                 entry = lst[1] or lst[2]
-                rest = Node.from_list(entry, is_monotone)
+                rest = Formula.from_list(entry, is_monotone)
                 node = NotNode(rest)
                 rest.set_parent(node)
                 return node
@@ -72,8 +116,8 @@ class Node:
                 if lst[1] == None or lst[2] == None:
                     raise ValueError("And of non-existent literal")
 
-                left = Node.from_list(lst[1], is_monotone)
-                right = Node.from_list(lst[2], is_monotone)
+                left = Formula.from_list(lst[1], is_monotone)
+                right = Formula.from_list(lst[2], is_monotone)
                 node = AndNode(left, right)
                 left.set_parent(node)
                 right.set_parent(node)
@@ -82,8 +126,8 @@ class Node:
             elif lst[0] == '|'  or lst[0] == 'or':
                 if lst[1] == None or lst[2] == None:
                     raise ValueError("Or of non-existent literal")
-                left = Node.from_list(lst[1], is_monotone)
-                right = Node.from_list(lst[2], is_monotone)
+                left = Formula.from_list(lst[1], is_monotone)
+                right = Formula.from_list(lst[2], is_monotone)
                 node = OrNode(left, right)
                 left.set_parent(node)
                 right.set_parent(node)
@@ -94,7 +138,7 @@ class Node:
         else:
             raise ValueError("Invalid input formula")
 
-class AndNode(Node):
+class AndNode(Formula):
     def __init__(self, left, right) -> None:
         super().__init__(NodeType.And, "&", left, right);
 
@@ -113,7 +157,16 @@ class AndNode(Node):
 
         return f"{left} & {right}"
 
-class OrNode(Node):
+    def set_span(self, val):
+        super().set_span(val)
+        left = list(val)
+        right = [0]*len(val)
+        left.append(1)
+        right.append(-1)
+        self._left.set_span(left)
+        self._right.set_span(right)
+
+class OrNode(Formula):
     def __init__(self, left, right) -> None:
         super().__init__(NodeType.Or, "|", left, right);
 
@@ -132,7 +185,12 @@ class OrNode(Node):
 
         return f"{left} | {right}"
 
-class NotNode(Node):
+    def set_span(self, val):
+        super().set_span(val)
+        self._left.set_span(val)
+        self._right.set_span(val)
+
+class NotNode(Formula):
     def __init__(self, value) -> None:
         super().__init__(NodeType.Not, "~", value, None);
 
@@ -145,31 +203,98 @@ class NotNode(Node):
 
         return f"~{left}"
 
-class LiteralNode(Node):
+    def set_span(self, val):
+        raise ValueError("Attempt to span for not gate")
+
+class LiteralNode(Formula):
     RESERVED = ['&', 'and', '|' , "or", "not" , "~"]
 
     def __init__(self, name: str) -> None:
         if name in LiteralNode.RESERVED:
             raise ValueError(f"Invalid name of literal: {name}")
         super().__init__(NodeType.Literal, name, None, None)
+        self._label = 0
+
+    def label(self):
+        return self._label
+
+    def set_label(self, label):
+        self._label = label
+
+    def unique_name(self):
+        return f"{self.name()}{self.label()}"
 
     def __repr__(self) -> str:
-        return f"{self.name()}"
+        if self.span():
+            return f"{self.name()}{self.label()}/{self.span()}"
+        else:
+            return f"{self.name()}{self.label()}"
+
+
+class MSP:
+    def __init__(self, formula: str, ring = ZZ):
+        node = Formula.from_formula(formula, True)
+        attribues = node.literals()
+        max_column = 0
+        matrix_rows = list()
+        pi = dict()
+        row_count = 0
+
+        for a in attribues:
+            if len(a.span()) > max_column:
+                max_column = len(a.span())
+
+        for a in attribues:
+            delta = max_column - len(a.span())
+            span = list(a.span())
+            if delta > 0:
+                span.extend([0]*delta)
+            matrix_rows.append(span)
+            pi[a.unique_name()] = row_count
+            row_count = row_count + 1
+
+        self._node = node
+        self._pi_inv = pi
+        self._matrix = Matrix(ring, matrix_rows)
+
+    def matrix(self):
+        return self._matrix
+
+    def node(self):
+        return self._node
+
+    def pi(self, index):
+        for (lit,val) in enumerate(self._pi_inv):
+            if val == index:
+                return lit
+        raise ValueError("Index not found")
+
+    def pi_inverse(self, literal):
+        return self._pi_inv[literal]
+
 
 if __name__ == '__main__':
     def main():
         f = "a&((b|c)^a->c)<->b"
-        tree = Node.from_formula(f)
+        tree = Formula.from_formula(f)
+        print(f"{tree}")
+        Formula.relabel_duplicates(tree)
         print(f"{tree}")
 
         try:
             f = "(a & b) | (b & ~c)"
-            tree = Node.from_formula(f, is_monotone=True)
+            tree = Formula.from_formula(f, is_monotone=True)
             print("Monotonicity check failed");
         except ValueError:
             pass
 
         g = "(a & b) | (b & c) | (c & a)"
-        tree = Node.from_formula(g, is_monotone=True)
+        tree = Formula.from_formula(g, is_monotone=True)
         print(f"{tree}")
+        Formula.relabel_duplicates(tree)
+        print(tree)
+
+        mat = MSP(g, GF(17))
+        print(mat.matrix())
+
     main()
